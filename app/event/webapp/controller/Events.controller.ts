86f27401b5a3dc187891668004dcd8a122cd7cb8
proallone <proallone14@gmail.com>
2025-03-28 22:34:15 +0100

    add popover

--- app/event/webapp/controller/Events.controller.ts
+++ app/event/webapp/controller/Events.controller.ts
@@ -1,11 +1,16 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import { ListItemBase$PressEvent } from "sap/m/ListItemBase";
+import { Link$PressEvent } from "sap/m/Link";
+import Dialog from "sap/m/Dialog";
+import Popover from "sap/m/Popover";
 
 /**
  * @namespace com.proallone.event.controller
  */
 export default class Events extends Controller {
+  protected eventPlacePopover: Popover;
+
   public onInit() {
     console.log("init");
   }
@@ -15,5 +20,17 @@ export default class Events extends Controller {
     const item = evt.getSource();
     const ID: string = item.getBindingContext()?.getProperty("ID");
     router.navTo("RouteEventDetails", { ID: ID });
+  }
+
+  public async onPlacePress(evt: Link$PressEvent) {
+    const src = evt.getSource();
+    const bindingPath = src.getBindingContext()?.getPath();
+
+    if(!this.eventPlacePopover) {
+      this.eventPlacePopover = await this.loadFragment({ name: "com.proallone.event.view.fragments.EventPlace"}) as Popover;
+    };
+
+    this.eventPlacePopover.bindElement(bindingPath!)
+    this.eventPlacePopover.openBy(src)
   }
 }

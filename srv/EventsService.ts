7143133ad169a81b80e89116d92a6aad0e232d7f
proallone <proallone14@gmail.com>
2025-04-03 20:30:53 +0200

    i18n adjustments

--- srv/EventsService.ts
+++ srv/EventsService.ts
@@ -1,26 +1,33 @@
 import cds from "@sap/cds";
-import { Events, Event, Users, EventLikes, EventCreated } from "#cds-models/EventsService";
+import {
+  Events,
+  Event,
+  Users,
+  EventLikes,
+  EventCreated,
+} from "#cds-models/EventsService";
 
 export class EventsService extends cds.ApplicationService {
   init() {
     const { like } = Event.actions;
 
     this.on(like, async (req) => {
-      const [ID] = req.params;
-      const { id } = req.user;
+      const [eventID] = req.params;
+      const { id: userEmail } = req.user;
       const user = await SELECT.one
         .from(Users)
         .columns("ID")
-        .where({ email: id });
-      if (user)
-        return await INSERT({ user_ID: user?.ID, event_ID: ID }).into(
-          EventLikes
-        );
-      return req.error(404, "User not found!");
+        .where({ email: userEmail });
+
+      if (!user) return req.error(404, "User not found!");
+
+      return await INSERT({ user_ID: user?.ID, event_ID: eventID }).into(
+        EventLikes
+      );
     });
 
     this.after("CREATE", Events, async (res) => {
-      this.emit(EventCreated, res?.ID)
+      this.emit(EventCreated, res?.ID);
     });
 
     this.on("READ", Event, async (req, next) => {
@@ -29,11 +36,11 @@ export class EventsService extends cds.ApplicationService {
       return next();
     });
 
-    this.on(EventCreated, (data)=>{
-      const   ID : string   = data.data;
+    this.on(EventCreated, (req) => {
+      const ID: string = req.data;
       console.info(`New event with ID ${ID} created!`);
-      //TODO add implementation? maybe mailing?
-    })
+      //TODO add implementation? maybe mailing? notification?
+    });
 
     return super.init();
   }

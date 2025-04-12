54f583dcb448c187a5a3d25bbf4dc0568a6e93ab
proallone <proallone14@gmail.com>
2025-04-12 14:52:23 +0200

    commit

--- srv/NotificationService.ts
+++ srv/NotificationService.ts
@@ -7,12 +7,13 @@ export class NotificationService extends cds.ApplicationService {
 
         this.on(sendNotification, async (req) => {
             const { eventID } = req.data;
-            console.log(eventID);
+            
             const users = await SELECT.from(Users).where({ notification_subscription : true});
             const event = await SELECT.one.from(Events, eventID!);
 
             const newNotif : Notification[] = [];
 
+            //TODO this is a placeholder
             for( const user of users){
                 newNotif.push({
                      title: `New event ${event?.name}`,
